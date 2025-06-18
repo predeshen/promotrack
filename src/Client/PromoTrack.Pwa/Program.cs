755eb403de4d4f01c3282411b66e23e7dcecdcec
Predeshen Naidoo <predeshen.naidoo@chillisoft.co.za>
2025-06-18 15:14:31 +0200

    adding docker support

--- src/Client/PromoTrack.Pwa/Program.cs
+++ src/Client/PromoTrack.Pwa/Program.cs
@@ -8,11 +8,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// --- This is the part that needs to change ---
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:7276/")
+    // Use the port we exposed for the API in docker-compose.yml
+    BaseAddress = new Uri("https://localhost:8081/")
 });
 
+
+// The rest of the file remains the same
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthService>();
